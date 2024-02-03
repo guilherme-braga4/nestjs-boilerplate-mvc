13b71d6e902efeaa7f02efd1839d697d87e3eed2
guilherme-braga4 <bragaguilherme152@yahoo.com.br>
2024-02-02 22:25:25 -0300

    test: comment to doc the test steps

--- src/example/example.controller.spec.ts
+++ src/example/example.controller.spec.ts
@@ -5,17 +5,27 @@ import { ExampleService } from './example.service';
 describe('ExampleController', () => {
   let controller: ExampleController;
 
+  //Config the controller or injectables (service/repository)
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ExampleController],
     }).compile();
 
+    //Instance of controller or injectables
     controller = module.get<ExampleController>(ExampleController);
   });
 
+  //Verify if controller/injectables instance are valid
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
 
-  it('should return a list of examples');
+  //Bellow is following all tests related to the specific business rules
+  /* Steps: 
+    Red: Write a failing test. This test should capture the behavior you're implementing but does not have corresponding code to make it pass.
+    Green: Write the minimum code necessary to make the failing test pass. Focus on just making the test pass without adding unnecessary complexity.
+    Blue: Refactor the code. After the test is passing, you can refactor the code to improve its structure, readability, or efficiency. Ensure the test continues to pass after refactoring.
+  */
+
+  it('should list all examples', () => {});
 });
