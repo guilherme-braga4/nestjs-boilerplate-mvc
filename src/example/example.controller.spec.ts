72dd4589bff540b323170e0b98a5a60ba4332e2a
guilherme-braga4 <bragaguilherme152@yahoo.com.br>
2024-02-02 22:03:44 -0300

    test: config the test for the ExampleController

--- src/example/example.controller.spec.ts
+++ src/example/example.controller.spec.ts
@@ -1,3 +1,18 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ExampleController } from './example.controller';
 import { ExampleService } from './example.service';
+
+describe('ExampleController', () => {
+  let controller: ExampleController;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [ExampleController],
+    }).compile();
+
+    controller = module.get<ExampleController>(ExampleController);
+  });
+
+  it('should be defined');
+  it('should return a list of examples');
+});
