cc996465ade9787021293e3f673a153486278cf3
guidev13 <guilhermebraga@valeti.com>
2024-03-18 17:37:34 -0300

    feat: controller - basic assertions to unit tests of listAll method

---
+++ src/example/mocks/mock.ts
@@ -0,0 +1,9 @@
+export const mockExamples = [{
+    id: 1,
+    name: 'example',
+    description: 'this is an example'
+}, {
+    id: 2,
+    name: 'example2',
+    description: 'this is an example2'
+}]
