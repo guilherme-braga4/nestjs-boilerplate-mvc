30bbca33cf95bc0b916f7159e90b7ef7b10b196b
guidev13 <guilhermebraga@valeti.com>
2024-04-10 10:17:46 -0300

    feat: swagger docs

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
 import { MainModule } from './main.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(MainModule);
+  const config = new DocumentBuilder()
+    .setTitle('API Example')
+    .setDescription('The example API description')
+    .setVersion('1.0.0')
+    .addTag('examples')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   const configService = app.get(ConfigService);
   const port = configService.get<number>('PORT') || 3000;
   app.enableCors();
