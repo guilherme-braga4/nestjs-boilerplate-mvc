2a8aa7435011743b97a1e6f4f05f66a1da85e6e9
guidev13 <guilhermebraga@valeti.com>
2024-04-11 13:18:25 -0300

    feat: crud service implementation on exampleController

--- src/modules/example/example.controller.ts
+++ src/modules/example/example.controller.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Controller, Get } from '@nestjs/common';
+import { BadRequestException, Controller, Get, Post, Param, Body, Put, Delete } from '@nestjs/common';
 import { ExampleServiceInterface } from './interfaces/example.interface';
 
 @Controller('example')
@@ -8,5 +8,25 @@ export class ExampleController {
   @Get()
   async getAllExamples(): Promise<any> {
     return await this.exampleService.listAllExamples();
+  }
+
+  @Get(':id')
+  async getExampleById(@Param('id') id: string): Promise<object> {
+    return await this.exampleService.listExampleById(id);
+  }
+
+  @Post()
+  async createExample(@Body() dto: any): Promise<object> {
+    return await this.exampleService.createExample(dto);
+  }
+
+  @Put(':id')
+  async updateExample(@Param('id') id: string, @Body() dto: any): Promise<object> {
+    return await this.exampleService.updateExample(id, dto);
+  }
+
+  @Delete(':id')
+  async deleteExample(@Param('id') id: string): Promise<void> {
+    return await this.exampleService.deleteExample(id);
   }
 }
