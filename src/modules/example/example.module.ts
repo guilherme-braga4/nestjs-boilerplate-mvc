f5d3617579b7c80041f9e03783f33c2834e1afcc
guidev13 <guilhermebraga@valeti.com>
2024-04-11 13:04:37 -0300

    feat: declare the exampleRepository custom provider

--- src/modules/example/example.module.ts
+++ src/modules/example/example.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
 import { ExampleController } from './example.controller';
 import { ExampleService } from './example.service';
+import { ExampleRepository } from './repositories/example.repository';
 
 @Module({
   imports: [],
   controllers: [ExampleController],
-  providers: [ExampleService],
+  providers: [
+    ExampleService,
+    ExampleRepository,
+    {
+      provide: 'ExampleRepositoryInterface', // Token de injeção como string
+      useClass: ExampleRepository,
+    },
+  ],
+  exports: [ExampleService]
 })
-export class ExampleModule {}
+export class ExampleModule { }
