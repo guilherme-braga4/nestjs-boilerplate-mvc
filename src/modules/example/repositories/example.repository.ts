a866572e8eb2b21a0d5381c7ada09979423c93a3
guidev13 <guilhermebraga@valeti.com>
2024-04-11 07:48:24 -0300

    feat: Example Repository

--- src/modules/example/repositories/example.repository.ts
+++ src/modules/example/repositories/example.repository.ts
@@ -1,9 +1,32 @@
 import { AbstractRepository } from "src/repositories/abstract.repository";
+import { PrismaRepository } from "src/repositories/prisma.repository";
+import { Example } from '@prisma/client'
+import { Injectable } from "@nestjs/common";
+import { PrismaService } from "src/database/prisma.service";
 
-export class ExampleRepository extends AbstractRepository {
-    super() { }
+@Injectable()
+export class ExampleRepository extends PrismaRepository<Example> {
+    constructor(prisma: PrismaService) {
+        super('Example', prisma);
+    }
 
     async findAllExamples() {
         return this.findAll()
+    }
+
+    async findExampleById(id: string) {
+        return this.findById(id)
+    }
+
+    async createExample(data: Example) {
+        return this.create(data)
+    }
+
+    async updateExample(id: string, data: Partial<Example>) {
+        return this.update(id, data)
+    }
+
+    async deleteExample(id: string) {
+        return this.delete(id)
     }
 }
