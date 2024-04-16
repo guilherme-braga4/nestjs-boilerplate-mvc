39796b8f9aaea96f909b3b74bdf98384bffba506
guidev13 <guilhermebraga@valeti.com>
2024-04-16 14:16:29 -0300

    chore: document dto

--- src/modules/example/dtos/example.dto.ts
+++ src/modules/example/dtos/example.dto.ts
@@ -2,12 +2,14 @@
 import { IsNotEmpty, IsString } from 'class-validator'
 import { PartialType } from '@nestjs/mapped-types';
 
+//DTO to validate input data from request from client
 export class RequestExampleDto {
     @IsString()
     @IsNotEmpty()
     readonly name: string
 }
 
+//DTO to response only the necessary output data to client
 export class ResponseExampleDto {
     readonly id: number;
     readonly name: string;
@@ -18,4 +20,7 @@ export class ResponseExampleDto {
     }
 }
 
+//DTO to update any attribute that has been created before
 export class UpdateExampleDto extends PartialType(RequestExampleDto) { }
+
+//Others DTOs should be created to another scenarios
